dea7cb638897436afdfbb89fdc100ff83bf246b7
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-08-31 11:21:40 +0530

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,12 @@ echo "Starting Magma Build..."
 
 git clone https://github.com/magma/magma.git --depth 1
 
-MAGMA_ROOT=$PWD/magma
-PUBLISH=$MAGMA_ROOT/orc8r/tools/docker/publish.sh
+MAGMA_ROOT=${PWD}/magma
+PUBLISH=${MAGMA_ROOT}/orc8r/tools/docker/publish.sh
 REGISTRY=shubhamtatvamasi
 MAGMA_TAG=$(date +%m-%d-%Y-%s)
 
-echo $MAGMA_ROOT
-echo $PUBLISH
-echo $REGISTRY
-echo $MAGMA_TAG
-
-ls $MAGMA_ROOT
+echo "Building controller and nginx docker image:"
+cd ${MAGMA_ROOT}/orc8r/cloud/docker
+./build.py -a
 
