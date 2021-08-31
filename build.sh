8d096e366bb7f50be48653f632e5e0e167ab6cc7
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-08-31 13:35:27 +0530

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -17,18 +17,25 @@ sed -i '65,71d' ${PUBLISH}
 cd ${MAGMA_ROOT}/orc8r/cloud/docker
 ./build.py --all
 
+# Pushing controller and nginx docker images:
+for image in controller nginx
+do
+  ${PUBLISH} -r ${REGISTRY} -i ${image} -v ${MAGMA_TAG}
+done
+
 # Building NMS docker image:
 cd ${MAGMA_ROOT}/nms/packages/magmalte
 docker-compose build magmalte
+
+# Pushing NMS docker image:
+COMPOSE_PROJECT_NAME=magmalte ${PUBLISH} -r ${REGISTRY} -i magmalte -v ${MAGMA_TAG}
 
 # Building Federation Gateway docker images:
 cd ${MAGMA_ROOT}/feg/gateway/docker
 docker-compose build --parallel
 
-magma_images=(controller nginx magmalte gateway_python gateway_go)
-
-# Pushing magma docker images:
-for image in ${magma_images}
+# Pushing Federation Gateway docker images:
+for image in gateway_python gateway_go
 do
   ${PUBLISH} -r ${REGISTRY} -i ${image} -v ${MAGMA_TAG}
 done
