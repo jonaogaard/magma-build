e45032c850c810a82d2bf016d51d618bfdb7e77b
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-08-31 13:00:18 +0530

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -7,25 +7,25 @@ MAGMA_ROOT=${PWD}/magma
 PUBLISH=${MAGMA_ROOT}/orc8r/tools/docker/publish.sh
 MAGMA_TAG=$(date +%m-%d-%Y--%H-%M-%S)
 
-echo "Cloning magma repo:"
+# Cloning magma repo:
 git clone https://github.com/magma/magma.git --depth 1
 
-echo "Deleting docker login code block:"
+# Deleting docker login code block:
 sed -i '65,71d' ${PUBLISH}
 
-echo "Building controller and nginx docker images:"
+# Building controller and nginx docker images:
 cd ${MAGMA_ROOT}/orc8r/cloud/docker
 ./build.py --all
 
-echo "Pushing controller and nginx docker images:"
+# Pushing controller and nginx docker images:
 for image in controller nginx
 do
   ${PUBLISH} -r ${REGISTRY} -i ${image} -v ${MAGMA_TAG}
 done
 
-echo "Building NMS docker image:"
+# Building NMS docker image:
 cd ${MAGMA_ROOT}/nms/packages/magmalte
 docker-compose build magmalte
 
-echo "Pushing NMS docker image:"
+# Pushing NMS docker image:
 COMPOSE_PROJECT_NAME=magmalte ${PUBLISH} -r ${REGISTRY} -i magmalte -v ${MAGMA_TAG}
